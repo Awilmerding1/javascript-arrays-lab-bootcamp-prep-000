bc85b92a9485bcac48eb67f2a7e1720cd7a44026
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-02-08 03:26:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const app = "I don't do much.";
 function destructivelyAppendKitten(name) {
-  name = kittens.push(name)
+  name = kittens.push(name);
   return name;
 }
 function destructivelyPrependKitten(name) {
-  name = kittens.pop(name)
+  name = kittens.pop(name);
   return name;
 }
   
